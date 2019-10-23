40a3db64ed52d4c282f56a44aade7c2660808319
kmuga001 <43682496+kmuga001@users.noreply.github.com>
2019-10-23 13:06:40 -0700

    created single input connector's file

---
+++ SingleInputConnector.h
@@ -0,0 +1,38 @@
+#ifndef SINGLEINPUTCONNECTOR_H
+#define SINGLEINPUTCONNECTOR_H
+
+#include "Command.h"
+#include <string>
+
+class SingleInputConnector : public Command {
+    public:
+    SingleInputConnector(string input) : Command(input){}
+
+    int parseStr(int index){
+        int i; /* returns the index value of the char value after ;*/
+        for(i = index; i < commandVal.size(); ++i){
+            if(commandVal.at(i) == '<'){
+                return i + 1;
+            }
+        }
+        return i;
+
+    }
+
+    bool isArgType() {
+        return false;
+    }
+
+    string next(int index) { /*gives the remaining string after parsing*/
+        string remaining = "";
+        return commandVal.substr(index);
+    }
+
+
+     string getFullString(int i, int j){
+         return "<";
+     }
+
+};
+
+#endif
