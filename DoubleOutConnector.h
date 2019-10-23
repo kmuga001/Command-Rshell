3bcf96382d46ec3a9e37075c86d0ae389d54fdfd
kmuga001 <43682496+kmuga001@users.noreply.github.com>
2019-10-23 13:01:39 -0700

    created DoubleOutConnector.h

---
+++ DoubleOutConnector.h
@@ -0,0 +1,43 @@
+#ifndef DOUBLEOUTCONNECTOR_H
+#define DOUBLEOUTCONNECTOR_H
+
+#include "Command.h"
+#include <string>
+
+class DoubleOutConnector : public Command { /*Connector class does not need to exist*/
+    public:
+        DoubleOutConnector(string input) : Command(input) {}
+
+        int parseStr(int index) {
+            /*char str[] = commandVal;
+ *             char* arg = strtok(str, " ");
+ *                         int i = index; //need to go through the char index in the arg 
+ *                                      now we have the index of the && operator*/
+            int i;
+            for(i = index; i < commandVal.size(); i++){
+                if(commandVal.at(i) == '>' && commandVal.at(i+1) == '>' ){
+                    return (i+2);
+                }
+
+            }
+
+            return i;
+        }
+
+        bool isArgType() {
+            return false;
+        }
+
+        string next(int index) { /*gives the remaining string after parsing*/
+            string remaining = "";
+            for(int i = index; i < commandVal.size(); i++){
+                remaining += commandVal.at(i);
+            }
+            return remaining;
+        }
+
+        string getFullString(int i, int j){
+            return ">>";
+        }
+};
+#endif
